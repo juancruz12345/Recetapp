1427f16cdde598d82259e2ef7a8db1019a9c5296
juan_cruz <urbanjuancruz@gmail.com>
2025-03-05 15:13:05 -0300

    fix promt

--- frontend/recetapp/components/Recipes.jsx
+++ frontend/recetapp/components/Recipes.jsx
@@ -57,7 +57,7 @@ export default function Recipes() {
           coockingTime:e.recipe.split('\n')?.filter(e=>e.includes('Cocción') || e.includes('Tiempo de cocción')|| e.includes('Tiempo de coccion')||e.includes('Coccion')).toString().match(/\d+/)[0],
           instructions:e.recipe.match(/\d+\.\s*.+?(?=\n\d+\.\s|\n*$)/gs),
           
-          ingredients:e.recipe?.split('\n').filter(line => (line.startsWith('-'))).join('\n').replaceAll('-','').split('\n'),
+          ingredients:e.recipe?.split('\n').filter(line => line.trimStart().startsWith('-')).join('\n').replaceAll('-','').split('\n'),
           portions:e.recipe?.split('\n').filter(e=>e.includes('Porciones'))?.toString().replaceAll('*','').replaceAll('-','')
           
         })
